1531ba31b1b2e1cc5ef942d509315d2b2e4d1091
Kai Aldag <trainerkai@yahoo.com>
2014-09-02 12:26:37 -0700

    initial commit

---
+++ Mixta/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mixta
+//
+//  Created by kai don aldag on 2014-09-02.
+//  Copyright (c) 2014 kai.don.aldag. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ MixtaTests/MixtaTests.m
@@ -0,0 +1,34 @@
+//
+//  MixtaTests.m
+//  MixtaTests
+//
+//  Created by kai don aldag on 2014-09-02.
+//  Copyright (c) 2014 kai.don.aldag. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MixtaTests : XCTestCase
+
+@end
+
+@implementation MixtaTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
